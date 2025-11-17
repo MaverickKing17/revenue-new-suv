53adf9930d8811c1b30c6b78c3219266976a4ac2
MaverickKing17 <kingnarmer702@gmail.com>
2025-11-17 15:32:52 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,24 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-<QueryClientProvider client={queryClient}>
-<TooltipProvider>
-<Toaster />
-<Sonner />
-<BrowserRouter>
-{/* Header is now part of every route /}
-<SiteHeader />
-<Routes>
-<Route path="/" element={<Index />} />
-{/ ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE /}
-<Route path="" element={<NotFound />} />
-</Routes>
-</BrowserRouter>
-</TooltipProvider>
-</QueryClientProvider>
-);
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          {/* Header is now part of every route */}
+          <SiteHeader />
+          <Routes>
+            <Route path="/" element={<Index />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
 
 export default App;
